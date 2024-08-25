e661313faa021754290e7982659dae3ebd037683
Rickie Karp <rickie.karp@yandex.com>
2024-08-25 15:32:56 +0200

    Add token to fetch request

--- install.sh
+++ install.sh
@@ -1,10 +1,17 @@
 #!/bin/bash -e
-if [ -f wpa_supplicant.conf ]; then
+
+tokenFile="initramfs-tools/token.conf"
+if [ ! -f $tokenFile ]; then
+  uuid=$(openssl rand -hex 16)
+  echo $uuid > $tokenFile
+fi
+
+if [ -f initramfs-tools/wpa_supplicant.conf ]; then
   cp -r initramfs-tools /etc/
-  cp wpa_supplicant.conf /etc/initramfs-tools
   cp bin/luksunlockusb /bin/luksunlockusb
   update-initramfs -u
 else
-  echo "Error: wpa_supplicant.conf not found, aborting installation"
+  echo "Error: initramfs-tools/wpa_supplicant.conf not found, aborting installation"
   echo "You can create wpa_supplicant.conf by using wpa_supplicant.conf.example as a template."
+  exit 1
 fi
