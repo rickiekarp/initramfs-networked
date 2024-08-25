0209edba3289d74cb90e488f483c2d0f97f485ab
Rickie Karp <rickie.karp@yandex.com>
2024-08-25 15:58:44 +0200

    Update file paths

--- install.sh
+++ install.sh
@@ -1,17 +1,19 @@
 #!/bin/bash -e
 
-tokenFile="initramfs-tools/token.conf"
+tokenFile="token.conf"
 if [ ! -f $tokenFile ]; then
   uuid=$(openssl rand -hex 16)
   echo $uuid > $tokenFile
 fi
 
-if [ -f initramfs-tools/wpa_supplicant.conf ]; then
+if [ -f wpa_supplicant.conf ]; then
   cp -r initramfs-tools /etc/
+  cp token.conf /etc/initramfs-tools
+  cp wpa_supplicant.conf /etc/initramfs-tools
   cp bin/luksunlockusb /bin/luksunlockusb
   update-initramfs -u
 else
-  echo "Error: initramfs-tools/wpa_supplicant.conf not found, aborting installation"
+  echo "Error: wpa_supplicant.conf not found, aborting installation"
   echo "You can create wpa_supplicant.conf by using wpa_supplicant.conf.example as a template."
   exit 1
 fi
